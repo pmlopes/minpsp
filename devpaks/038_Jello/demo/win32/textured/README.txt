19e89074e0e9ed90eb89dc575d1fbe622ff28c7a
pmml <pmml@f6e5ea7c-4b97-44d9-91af-b37c84cff242>
2008-07-04 07:15:56 +0000

    updated to external svn JelloPhysics server
    git-svn-id: http://svn.code.sf.net/p/minpspw/code/trunk@28 f6e5ea7c-4b97-44d9-91af-b37c84cff242

--- devpaks/038_Jello/demo/win32/textured/README.txt
+++
@@ -1,3 +0,0 @@
-Here is some quick textured demo - not very nice code... It will be changed.
-
-Keys: F2,F3,F4
--- devpaks/038_Jello/devpak.sh
+++ devpaks/038_Jello/devpak.sh
@@ -2,7 +2,21 @@
 
 LIBNAME=Jello
 
-cd $LIBNAME
+if [ ! -d PSP_JellyPhysic ]
+then
+	svn checkout http://jphysicmod.googlecode.com/svn/trunk/PSP_JellyPhysic || { echo "ERROR GETTING $LIBNAME"; exit 1; }
+else
+	svn update PSP_JellyPhysic
+fi
+
+if [ ! -f $LIBNAME-patched ]
+then
+	patch -p0 -d PSP_JellyPhysic -i ../$LIBNAME.patch || { echo "Error patching $LIBNAME"; exit; }
+	touch $LIBNAME-patched
+fi
+
+cd PSP_JellyPhysic
+
 if [ ! -f $LIBNAME-build ]
 then
 	make || { echo "Error building $LIBNAME"; exit 1; }
