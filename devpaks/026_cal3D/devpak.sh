f6c68809203edbbc53946e6b6094c137e75da5f5
pmml <pmml@f6e5ea7c-4b97-44d9-91af-b37c84cff242>
2010-06-21 20:38:26 +0000

    fix again for linux
    git-svn-id: http://svn.code.sf.net/p/minpspw/code/trunk@155 f6e5ea7c-4b97-44d9-91af-b37c84cff242

--- devpaks/026_cal3D/devpak.sh
+++ devpaks/026_cal3D/devpak.sh
@@ -9,9 +9,12 @@ svnGet build svn://svn.ps2dev.org/psp/trunk $LIBNAME
 
 cd build/$LIBNAME
 rm -Rf autom4te.cache
+mkdir -p ../build
+cd ../build
+
 LDFLAGS="-L$(psp-config --pspsdk-path)/lib" LIBS="-lstdc++ -lpsplibc -lpspuser" ../$LIBNAME/configure --host=psp --disable-shared --prefix=$(pwd)/../target/psp
 
-make
+LDFLAGS="-L$(psp-config --pspsdk-path)/lib" LIBS="-lstdc++ -lpsplibc -lpspuser" make
 make install 
 
 mkdir -p ../target/doc
