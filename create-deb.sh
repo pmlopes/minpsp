8dc0320386c7ba966fd2e37de587874c207e1c5d
pmml <pmml@f6e5ea7c-4b97-44d9-91af-b37c84cff242>
2010-06-23 12:11:40 +0000

    added basic deb package
    git-svn-id: http://svn.code.sf.net/p/minpspw/code/trunk@160 f6e5ea7c-4b97-44d9-91af-b37c84cff242

---
+++ create-deb.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+set -e
+
+VERSION=0.9.6
+
+rm -Rf minpspw-$VERSION || true
+mkdir minpspw-$VERSION
+chown root:root minpspw-$VERSION
+
+cd minpspw-$VERSION
+
+# create basic debian-binary
+echo 2.0 > debian-binary
+
+# create the data package
+mkdir -p opt
+cp -Rf $(pwd)/../../pspsdk opt
+chown -R root:root ./opt
+tar czf data.tar.gz ./opt
+
+# create control file
+cat >> control <<EOF
+Package: minpspw-pspsdk
+Version: 0.9.6
+Maintainer: Paulo Lopes <pmlopes@gmail.com>
+Installed-Size: `du -ks opt|cut -f 1`
+Priority: optional
+Homepage: http://www.jetdrone.com/
+Description: PSP Homebrew Development Kit 0.9.6
+ The MinPSPW is a development environment for building applications,
+ games, and components targetting the Playstation Portable.
+EOF
+
+cat >> preinst <<EOF
+#!/bin/sh
+set -e
+. /usr/share/debconf/confmodule
+exit 0
+EOF
+chmod a+x preinst
+
+cat >> postrm <<EOF
+#!/bin/sh
+set -e
+. /usr/share/debconf/confmodule
+exit 0
+EOF
+chmod a+x postrm
+
+tar czf control.tar.gz control preinst postrm
+
+# create deb
+ar -r ../minpspw_$VERSION-1ubuntu0.deb debian-binary control.tar.gz data.tar.gz
+
+cd ..
+
