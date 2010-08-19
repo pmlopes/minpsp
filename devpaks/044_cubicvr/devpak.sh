deaf589c2bedcca4866061844a41d6aff12e28ea
pmml <pmml@f6e5ea7c-4b97-44d9-91af-b37c84cff242>
2010-08-19 19:52:47 +0000

    updated cubicvr to download less stuff from the internet
    git-svn-id: http://svn.code.sf.net/p/minpspw/code/trunk@162 f6e5ea7c-4b97-44d9-91af-b37c84cff242

--- devpaks/044_cubicvr/devpak.sh
+++ devpaks/044_cubicvr/devpak.sh
@@ -5,9 +5,15 @@ set -e
 LIBNAME=cubicvr
 VERSION=1232
 
-svnGet build https://cubicvr.svn.sourceforge.net/svnroot/cubicvr/trunk cubicvr
+svnGet build https://cubicvr.svn.sourceforge.net/svnroot/cubicvr/trunk/cubicvr cubicvr
+svnGet build https://cubicvr.svn.sourceforge.net/svnroot/cubicvr/trunk/cubicvr psp
 
+mv build/cubicvr build/cubicvr_src
+mkdir -p build/$LIBNAME
+mv build/cubicvr_src build/$LIBNAME/cubicvr
+mv build/psp build/$LIBNAME/psp
 cd build/$LIBNAME/psp
+cp -f ../../../Makefile .
 make
 make install
 cd ../../..
