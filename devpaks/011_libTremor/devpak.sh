d19da1bb15cfbee4dd11ae3bb8f4afdcb7fa50b1
pmml <pmml@f6e5ea7c-4b97-44d9-91af-b37c84cff242>
2010-11-25 13:14:48 +0000

    Move from ps2dev svn to xiph.org since it is the official source
    git-svn-id: http://svn.code.sf.net/p/minpspw/code/trunk@189 f6e5ea7c-4b97-44d9-91af-b37c84cff242

--- devpaks/011_libTremor/devpak.sh
+++ devpaks/011_libTremor/devpak.sh
@@ -2,17 +2,17 @@
 set -e
 . ../util/util.sh
 
-LIBNAME=libTremor
+LIBNAME=Tremor
 VERSION=1.0.2
 
-svnGet build svn://svn.ps2dev.org/psp/trunk $LIBNAME
+svnGet build http://svn.xiph.org/trunk $LIBNAME
 
 cd build/$LIBNAME
-LDFLAGS="-L$(psp-config --pspsdk-path)/lib" LIBS="-lc -lpspuser" ./autogen.sh --host psp --prefix=$(pwd)/../target/psp
+LDFLAGS="-L$(psp-config --pspsdk-path)/lib" LIBS="-lc -lpspuser" ./autogen.sh --host=psp --prefix=$(pwd)/../target/psp
 
 make
-
 make install
+
 mkdir -p $(pwd)/../target/doc/$LIBNAME
 cp doc/*.html $(pwd)/../target/doc/$LIBNAME
 cp doc/*.css $(pwd)/../target/doc/$LIBNAME
@@ -22,4 +22,3 @@ cd ../..
 makeInstaller $LIBNAME $VERSION
 
 echo "Done!"
-
